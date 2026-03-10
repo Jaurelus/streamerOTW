ddf29d580f28ef8d43d174759d635165c3c4139e
“Jaurelus” <hrqrdbrdzf@privaterelay.appleid.com>
2026-03-09 21:18:49 -0400

    Gettin injury info

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 import os
+from modules.espnData import league
 
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="http://127.0.0.1:5500",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -17,6 +28,9 @@ def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
 
 app.mount("/static", StaticFiles(directory=os.path.join(os.path.dirname(__file__), "../frontend")), name="static")
+
+#Routes
+app.include_router(league.router, prefix="/streamers", tags=["streamers"])
 
 
 #@app.get("/")
